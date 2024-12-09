10250dbdf74dfd3bbcceadc400ec456e9f3d424b
Ashutoshh <ashutoshh@linux.com>
2024-12-09 16:15:30 +0530

    updates selenium

--- day2/automate.py
+++ day2/automate.py
@@ -12,7 +12,8 @@ chrome_driver.get("https://rahulshettyacademy.com/angularpractice/")
 
 # Selenium can find elements by number of things 
 # name , classname  , id , cssSelector , xpath 
-chrome_driver.find_element(By.NAME,"name").send_keys("ashutoshh")
+#chrome_driver.find_element(By.NAME,"name").send_keys("ashutoshh")
+chrome_driver.find_element(By.CSS_SELECTOR,"input[name='name']").send_keys("ashutoshh")
 chrome_driver.find_element(By.NAME,"email").send_keys("ashutoshh@linux.com")
 chrome_driver.find_element(By.ID,"exampleInputPassword1").send_keys("HelloCloud@123")
 # click the box 
@@ -22,9 +23,20 @@ my_select = Select(chrome_driver.find_element(By.ID,"exampleFormControlSelect1")
 # we can select data by index , visible text as well
 my_select.select_by_index(1)
 #my_select.select_by_visible_text("Female")
+# using css_selector for radio button 
+chrome_driver.find_element(By.CSS_SELECTOR,"#inlineRadio1").click()
+# using xpath for complex pattern matching 
+chrome_driver.find_element(By.XPATH,"//input[@type='submit']").click()
+# find the message data 
+message = chrome_driver.find_element(By.CLASS_NAME,"alert-success").text
 
 time.sleep(4)
 
+
+# printnt message
+print(message)
+# message validation  using assertion in python 
+assert "Success" in message
 # printing title 
 print("page title : ",chrome_driver.title)
 # current url 
