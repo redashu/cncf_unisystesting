ae992e12cc573fae60c0a7eb20ab5a588fa2ce65
Ashutoshh <ashutoshh@linux.com>
2024-12-09 12:51:27 +0530

    updates python check

--- day2/automate.py
+++ day2/automate.py
@@ -6,8 +6,16 @@ import time
 # initilizing web driver 
 chrome_driver = webdriver.Chrome()
 # opening a web url 
-chrome_driver.get("https://portal.adhocnet.org/")
+chrome_driver.get("https://rahulshettyacademy.com/angularpractice/")
 time.sleep(3)
+
+# printing title 
+print("page title : ",chrome_driver.title)
+# current url 
+print("page URL  : ",chrome_driver.current_url)
+# saving screenshot 
+chrome_driver.save_screenshot("pagehome1.png")
+print("current page screenshot saved")
 # closing my driver / stopping  
 chrome_driver.quit()
 
